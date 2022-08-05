af130c48516b6c5567d1e648f9af47218b70094a
Madalina <petrus_andreeamadalina@yahoo.com>
2022-08-05 16:40:31 +0300

    new

---
+++ AnimailRescue/Girl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimailRescue
+{
+    internal class Girl: Person
+    {
+        protected bool animalLover;
+        protected Animal pet;
+
+        public Girl(string name, int age, float height, string hairColor, bool animalLover) : base(name, age, height, hairColor)
+        {
+            this.animalLover = animalLover;
+        }
+        public Girl(string name, int age, float height, string hairColor,bool animalLover, Animal pet) : base(name, age, height, hairColor)
+        {
+            this.animalLover = animalLover;
+            this.pet = pet;
+        }
+
+        public bool GetAnimalLover()
+        {
+            return this.animalLover;
+        }
+
+        public void SetAnimalLover(bool AnimalLover)
+        {
+            this.animalLover = animalLover;
+        }
+
+
+        public Animal  GetPet()
+        {
+            return this.pet;
+        }
+
+        public void SetPet(Animal Pet)
+        {
+            this.pet = pet;
+        }
+    }
+}
---
+++ AnimailRescue/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimailRescue
+{
+    internal abstract class Person
+    {
+        protected string name;
+        protected int age;
+        protected float height;
+        protected string hairColor;
+
+        protected Person(string name, int age, float height, string hairColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.height = height;
+            this.hairColor = hairColor;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public int GetAge()
+        {
+            return this.age;
+        }
+
+        public void SetAge(int age)
+        {
+            this.age = age;
+        }
+
+        public float GetHeight()
+        {
+            return this.height;
+        }
+
+        public void SetHeight(float height)
+        {
+            this.height = height;
+        }
+
+        public string GetHairColor()
+        {
+            return this.hairColor;
+        }
+
+        public void SetHairColor(string hairColor)
+        {
+            this.hairColor = hairColor;
+        }
+    }
+}
